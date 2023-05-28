0d7bbbe6ff0759e3570ad1dba4f15aeceb2d22c4
Ina <ina.levankova@yandex.ru>
2023-05-28 18:09:10 +0200

    Web View controller with a navigation back button

--- Auth/AuthViewController.swift
+++ Auth/AuthViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
-class AuthViewControllet: UIViewController {
-    
+class AuthViewController: UIViewController {
+    private let shwoWebViewSegueIdentifier = String("ShowWebView")
+            
 }
---
+++ Auth/WebViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewController.swift
+//  ImageFeed
+//
+//  Created by Ina on 28/05/2023.
+//
+
+import Foundation
+import WebKit
+
+final class WebViewController: UIViewController {
+
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+    }
+}
